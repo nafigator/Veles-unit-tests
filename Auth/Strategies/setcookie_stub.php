e332270b45dfdc6915f6e4d14dd7e6356600185e
Yancharuk Alexander <alex@itvault.info>
2016-06-06 21:23:30 +0300

    Reimplement CookieStrategy unit-tests

--- Auth/Strategies/setcookie_stub.php
+++ Auth/Strategies/setcookie_stub.php
@@ -1,20 +1,21 @@
 <?php
 /**
- * File is using for stub error_log() PHP function
+ * File is using for stub set_cookie() PHP function
  *
- * @file      error_log_stub.php
+ * @file      setcookie_stub.php
  *
  * PHP version 5.4+
  *
  * @author    Yancharuk Alexander <alex at itvault dot info>
  * @copyright © 2012-2016 Alexander Yancharuk <alex at itvault at info>
- * @date      2016-06-03 21:41
+ * @date      2016-06-06 21:11
  * @license   The BSD 3-Clause License
  *            <https://tldrlegal.com/license/bsd-3-clause-license-(revised)>
  */
-namespace Veles\DataBase\Loggers;
 
-function error_log($message, $type, $path)
+namespace Veles\Auth\Strategies;
+
+function setcookie($id, $value, $expire, $path, $domain, $secure, $http_only)
 {
-	echo $message . $type . PHP_EOL . $path;
+	return true;
 }
