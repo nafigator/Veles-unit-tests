b90b32b49dcd79f703e64d8e2ba2a18bc35eedb3
Yancharuk Alexander <alex@itvault.info>
2016-06-07 13:17:48 +0300

    Update phpunit bootstraps

--- bootstrap-example.php
+++ bootstrap-example.php
@@ -20,16 +20,11 @@
 
 namespace Veles\Tests;
 
-use PDO;
 use Veles\AutoLoader;
 use Veles\Cache\Adapters\MemcacheAdapter;
 use Veles\Cache\Adapters\MemcachedAdapter;
 use Veles\Cache\Adapters\MemcacheRaw;
 use Veles\Cache\Cache;
-use Veles\DataBase\Adapters\PdoAdapter;
-use Veles\DataBase\ConnectionPools\ConnectionPool;
-use Veles\DataBase\Connections\PdoConnection;
-use Veles\DataBase\Db;
 use Veles\View\Adapters\NativeAdapter;
 use Veles\View\View;
 
@@ -57,35 +52,3 @@ MemcacheRaw::setConnectionParams('localhost', 11211);
 MemcachedAdapter::addCall('addServer', ['localhost', 11211]);
 MemcacheAdapter::addCall('addServer', ['localhost', 11211]);
 Cache::setAdapter(MemcachedAdapter::instance());
-
-// Parameters for Db connection
-$pool = new ConnectionPool();
-$conn1 = new PdoConnection('master');
-
-// Database "test" must be created
-$conn1->setDsn('mysql:host=localhost;dbname=test;charset=utf8')
-	->setUserName('user')
-	->setPassword('password')
-	->setOptions([
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode=ANSI'
-	]);
-
-// For testing exceptions thrown on connection errors
-$conn2 = new PdoConnection('fake');
-$conn2->setDsn('mysql:host=localhost;dbname=test;charset=utf8')
-	->setUserName('user')
-	->setPassword('password')
-	->setOptions([
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-	]);
-
-$pool->addConnection($conn1, true);
-$pool->addConnection($conn2);
-PdoAdapter::setPool($pool);
-
-Db::setAdapter(PdoAdapter::instance());
-system('mysql -u user -p password -e "DROP DATABASE IF EXISTS test; CREATE DATABASE IF NOT EXISTS test DEFAULT CHARACTER SET utf8"');
-register_shutdown_function(function() { Db::query('DROP DATABASE test'); });
--- travisci-nightly-bootstrap.php
+++ travisci-nightly-bootstrap.php
@@ -12,12 +12,7 @@
 
 namespace Veles\Tests;
 
-use PDO;
 use Veles\AutoLoader;
-use Veles\DataBase\Adapters\PdoAdapter;
-use Veles\DataBase\ConnectionPools\ConnectionPool;
-use Veles\DataBase\Connections\PdoConnection;
-use Veles\DataBase\Db;
 use Veles\View\Adapters\NativeAdapter;
 use Veles\View\View;
 
@@ -35,34 +30,3 @@ AutoLoader::registerPath(
 $view_adapter = new NativeAdapter;
 $view_adapter->setTemplateDir(TEST_DIR . '/Project/View/');
 View::setAdapter($view_adapter);
-
-// Parameters for Db connection
-$pool = new ConnectionPool();
-$conn1 = new PdoConnection('master');
-
-// Database "test" must be created
-$conn1->setDsn('mysql:host=localhost;dbname=test;charset=utf8')
-	->setUserName('travis')
-	->setPassword('')
-	->setOptions([
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode=ANSI'
-	]);
-
-// For testing exceptions thrown on connection errors
-$conn2 = new PdoConnection('fake');
-$conn2->setDsn('mysql:host=localhost;dbname=test;charset=utf8')
-	->setUserName('fake-user')
-	->setPassword('fake-password')
-	->setOptions([
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode=ANSI'
-	]);
-
-$pool->addConnection($conn1, true);
-$pool->addConnection($conn2);
-PdoAdapter::setPool($pool);
-
-Db::setAdapter(PdoAdapter::instance());
