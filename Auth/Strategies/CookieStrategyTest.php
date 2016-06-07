4bb20385da1d85c7864d5fce89213130096f5fe1
Yancharuk Alexander <alex@itvault.info>
2016-06-07 11:45:33 +0300

    Reimplement UserTest unit-tests

--- Auth/Strategies/CookieStrategyTest.php
+++ Auth/Strategies/CookieStrategyTest.php
@@ -14,6 +14,10 @@ require_once 'setcookie_stub.php';
  */
 class CookieStrategyTest extends \PHPUnit_Framework_TestCase
 {
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 */
 	protected function tearDown()
 	{
 		DbCopy::unsetAdapter();
--- Model/ActiveRecordTest.php
+++ Model/ActiveRecordTest.php
@@ -30,6 +30,10 @@ class ActiveRecordTest extends \PHPUnit_Framework_TestCase
 		$this->object = new ActiveRecord;
 	}
 
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 */
 	protected function tearDown()
 	{
 		DbCopy::unsetAdapter();
