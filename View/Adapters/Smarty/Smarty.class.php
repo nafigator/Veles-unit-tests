5311eba1a4e9d6790e61e09fc38d03944c246f59
Yancharuk Alexander <alex@itvault.info>
2016-06-08 08:30:25 +0300

    Added unit-tests for SmartyAdapter

---
+++ View/Adapters/Smarty/Smarty.class.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This is stub for Smarty class
+ *
+ * @file      Smarty.class.php
+ *
+ * PHP version 5.4+
+ *
+ * @author    Yancharuk Alexander <alex at itvault dot info>
+ * @copyright © 2012-2016 Alexander Yancharuk <alex at itvault at info>
+ * @date      2016-06-08 07:52
+ * @license   The BSD 3-Clause License
+ *            <https://tldrlegal.com/license/bsd-3-clause-license-(revised)>
+ */
+
+class Smarty
+{
+	public function assign($vars = [])
+	{
+		return true;
+	}
+
+	public function clearAssign(array $vars)
+	{
+		return true;
+	}
+
+	public function display($path)
+	{
+		return true;
+	}
+
+	public function fetch($path)
+	{
+		return true;
+	}
+
+	public function clearCache($tpl)
+	{
+		return true;
+	}
+
+	public function clearAllCache($exp_time = null)
+	{
+		return true;
+	}
+
+	public function isCached($tpl)
+	{
+		return true;
+	}
+}
