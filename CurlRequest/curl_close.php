e14e4c97cfd19cc2a357a169d44bb05a0f61e662
Yancharuk Alexander <alex@itvault.info>
2016-06-23 13:46:07 +0300

    Added unit-tests for CurlRequest class

---
+++ CurlRequest/curl_close.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is used for stubbing curl_close() function
+ *
+ * @file      curl_close.php
+ *
+ * PHP version 5.4+
+ *
+ * @author    Yancharuk Alexander <alex at itvault dot info>
+ * @copyright © 2012-2016 Alexander Yancharuk <alex at itvault at info>
+ * @date      2016-06-23 12:30
+ * @license   The BSD 3-Clause License
+ *            <https://tldrlegal.com/license/bsd-3-clause-license-(revised)>
+ */
+
+namespace Veles\CurlRequest;
+
+function curl_close($curl)
+{
+	if (!is_resource($curl)) {
+		throw new \InvalidArgumentException('$curl argument must be resource');
+	}
+}
---
+++ CurlRequest/curl_exec.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is used for stubbing curl_exec() function
+ *
+ * @file      curl_exec.php
+ *
+ * PHP version 5.4+
+ *
+ * @author    Yancharuk Alexander <alex at itvault dot info>
+ * @copyright © 2012-2016 Alexander Yancharuk <alex at itvault at info>
+ * @date      2016-06-23 12:54
+ * @license   The BSD 3-Clause License
+ *            <https://tldrlegal.com/license/bsd-3-clause-license-(revised)>
+ */
+
+namespace Veles\CurlRequest;
+
+function curl_exec($curl)
+{
+	if (!is_resource($curl)) {
+		throw new \InvalidArgumentException('$curl argument must be resource');
+	}
+
+	return 'This is curl_exec result!';
+}
