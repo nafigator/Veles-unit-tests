8fa5bc7956e96ba63f3a62c2b8b8ad637a3b9d08
Yancharuk Alexander <alex@itvault.info>
2016-06-23 11:20:35 +0300

    Small cleanup

--- CurlRequest/CurlAbstractTest.php
+++ CurlRequest/CurlAbstractTest.php
@@ -31,14 +31,6 @@ class CurlAbstractTest extends \PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * Tears down the fixture, for example, closes a network connection.
-	 * This method is called after a test is executed.
-	 */
-	protected function tearDown()
-	{
-	}
-
-	/**
 	 * @covers Veles\CurlRequest\CurlAbstract::getErrorCode
 	 */
 	public function testGetErrorCode()
