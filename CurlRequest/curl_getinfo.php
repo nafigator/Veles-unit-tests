061ce413456b9763b23fe32b472dede787f0a164
Yancharuk Alexander <alex@itvault.info>
2016-10-21 17:25:20 +0300

    Remove email duplication

--- CurlRequest/curl_getinfo.php
+++ CurlRequest/curl_getinfo.php
@@ -7,7 +7,7 @@
  * PHP version 5.4+
  *
  * @author    Yancharuk Alexander <alex at itvault dot info>
- * @copyright © 2012-2016 Alexander Yancharuk <alex at itvault at info>
+ * @copyright © 2012-2016 Alexander Yancharuk
  * @date      2016-06-23 11:01
  * @license   The BSD 3-Clause License
  *            <https://tldrlegal.com/license/bsd-3-clause-license-(revised)>
--- DataBase/Connections/PDOStub.php
+++ DataBase/Connections/PDOStub.php
@@ -7,7 +7,7 @@
  * PHP version 5.6+
  *
  * @author    Yancharuk Alexander <alex at itvault dot info>
- * @copyright © 2012-2016 Alexander Yancharuk <alex at itvault at info>
+ * @copyright © 2012-2016 Alexander Yancharuk
  * @date      2016-05-29 06:36
  * @license   The BSD 3-Clause License
  *            <https://tldrlegal.com/license/bsd-3-clause-license-(revised)>
@@ -18,7 +18,7 @@ namespace Veles\Tests\DataBase\Connections;
 /**
  * Class   PDOStub
  *
- * @author Yancharuk Alexander <alex at itvault at info>
+ * @author Yancharuk Alexander <alex at itvault dot info>
  */
 class PDOStub extends \PDO
 {
