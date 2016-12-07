c91f2e068b17743cfa69a25e4d3939ef059255d8
Yancharuk Alexander <alex@itvault.info>
2016-12-07 16:06:29 +0300

    Full coverage of NativeAdapter

---
+++ View/Adapters/NativeAdapterChild.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file      NativeAdapterChild.php
+ *
+ * PHP version 5.6+
+ *
+ * @author    Yancharuk Alexander <alex at itvault dot info>
+ * @copyright © 2012-2016 Alexander Yancharuk <alex at itvault at info>
+ * @date      2016-12-07 15:54
+ * @license   The BSD 3-Clause License
+ *            <https://tldrlegal.com/license/bsd-3-clause-license-(revised)>
+ */
+namespace Veles\Tests\View\Adapters;
+
+use Veles\View\Adapters\NativeAdapter;
+
+/**
+ * Class   NativeAdapterChild
+ *
+ * @author Yancharuk Alexander <alex at itvault at info>
+ */
+class NativeAdapterChild extends NativeAdapter
+{
+	public static function unsetInstance()
+	{
+		static::$instance = null;
+	}
+}
