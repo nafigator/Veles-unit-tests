dcca98143ab16e484cf47e4da53a5b77791f2f63
Yancharuk Alexander <alex@itvault.info>
2016-12-08 11:03:16 +0300

    Added unit-test for RestApiController

---
+++ Controllers/RestApiExampleController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file      RestApiExampleController.php
+ *
+ * PHP version 5.6+
+ *
+ * @author    Yancharuk Alexander <alex at itvault dot info>
+ * @copyright © 2012-2016 Alexander Yancharuk <alex at itvault at info>
+ * @date      2016-12-08 10:49
+ * @license   The BSD 3-Clause License
+ *            <https://tldrlegal.com/license/bsd-3-clause-license-(revised)>
+ */
+
+namespace Tests\Controllers;
+
+use Veles\Controllers\RestApiController;
+
+/**
+ * Class   RestApiExampleController
+ *
+ * @author Yancharuk Alexander <alex at itvault at info>
+ */
+class RestApiExampleController extends RestApiController
+{
+	public function get()
+	{
+		return [];
+	}
+
+	public function post()
+	{
+		return [];
+	}
+
+	public function put()
+	{
+		return [];
+	}
+
+	public function delete()
+	{
+		return [];
+	}
+}
