d78017ad8be6473b170521761470fa8829ddf796
Yancharuk Alexander <alex@itvault.info>
2017-03-18 13:12:11 +0300

    Update copy years

--- AutoLoaderFake.php
+++ AutoLoaderFake.php
@@ -6,7 +6,7 @@
  * PHP version 5.6+
  *
  * @author    Alexander Yancharuk <alex at itvault dot info>
- * @copyright © 2012-2016 Alexander Yancharuk
+ * @copyright © 2012-2017 Alexander Yancharuk
  * @date      Втр Янв 22 21:23:01 2013
  * @license   The BSD 3-Clause License
  *          <https://tldrlegal.com/license/bsd-3-clause-license-(revised)>.
