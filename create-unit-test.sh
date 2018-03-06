d3f126e9aa81288c2c5b961c3b28a18f46f9e12d
Yancharuk Alexander <alex@itvault.info>
2018-03-06 06:08:15 +0300

    Incapsulate functionality in create-unit-test.sh into main()

--- create-unit-test.sh
+++ create-unit-test.sh
@@ -2,71 +2,148 @@
 
 # Скрипт для создания скелета юнит-теста
 
-readonly CLASSES_DIR='/home/alex/veles'
-readonly UNIT_TESTS_DIR='/home/alex/veles/Tests'
-readonly CLASS_EXTENSION='.php'
-readonly VERSION='0.0.6'
-readonly CURRENT_DIR=$(pwd)
-readonly TEST_CASE_NAMESPACE='PHPUnit\Framework\TestCase'
-
 usage_help() {
-	print_version
-	cat <<EOL
-Usage: $0 [OPTION...] <Class name>
+	printf "${BOLD}Usage:${CLR}
+  create-unit-test.sh [OPTION...] <Class name>
 
-Options:
+${BOLD}Options:${CLR}
   -v, --version              Show script version
   -h, --help                 Show this help message
   -d, --debug                Run script in debug mode
   -n, --namespace            Set namespace for class
 
-Examples:
+${BOLD}Description:${CLR}
+  Script for creating unit-test skeleton
 
-./create-unit-test.sh --namespace='Veles\View' View
+${BOLD}Examples:${CLR}
+  ./create-unit-test.sh --namespace='Veles\View' View
+  ./create-unit-test.sh -d -n 'Veles\View' View
+  ./create-unit-test.sh --debug ButtonElement
 
-./create-unit-test.sh -d -n 'Veles\View' View
+"
 
-./create-unit-test.sh --debug ButtonElement
-
-EOL
-	exit 2;
+	return 0
 }
 
 print_version() {
-	cat <<EOL
-create-unit-test ${VERSION} by Yancharuk Alexander
+	printf "create-unit-test.sh ${BOLD}${VERSION}${CLR} by Yancharuk Alexander\n\n"
 
-EOL
+	return 0
 }
 
-# function for debug messages
-debug() {
-	[ ! -z ${DEBUG} ] && printf "[$(date --rfc-3339=seconds)]: \033[0;32mDEBUG:\033[0m $@\n"
+# Function for datetime output
+format_date() {
+	printf "$GRAY$(date +%Y-%m-%d\ %H:%M:%S)$CLR"
 }
 
-# Function for error
+# Function for error messages
 error() {
-	printf "[$(date --rfc-3339=seconds)]: \033[0;31mERROR:\033[0m $@\n" >&2
+	printf "[$(format_date)]: ${RED}ERROR:$CLR $@\n" >&2
+}
+
+# Function for informational messages
+inform() {
+	printf "[$(format_date)]: ${GREEN}INFO:$CLR $@\n"
 }
 
 # Function for warning messages
 warning() {
-	printf "[$(date --rfc-3339=seconds)]: \033[0;33mWARNING:\033[0m $@\n" >&2
+	printf "[$(format_date)]: ${YELLOW}WARNING:$CLR $@\n" >&2
 }
 
-check_dependencies() {
-	local commands='phpunit-skelgen printf sed find tr perl'
+# Function for debug messages
+debug() {
+	[ ! -z ${DEBUG} ] && printf "[$(format_date)]: ${GREEN}DEBUG:$CLR $@\n"
+}
+
+# Function for operation status
+#
+# Usage: status MESSAGE STATUS
+# Examples:
+# status 'Upload scripts' $?
+# status 'Run operation' OK
+status() {
+	if [ -z "$1" ] || [ -z "$2" ]; then
+		error "status(): not found required parameters!"
+		return 1
+	fi
+
 	local result=0
 
-	for i in ${commands}; do
+	if [ $2 = 'OK' ]; then
+		printf "[$(format_date)]: %-${status_length}b[$GREEN%s$CLR]\n" "$1" "OK"
+	elif [ $2 = 'FAIL' ]; then
+		printf "[$(format_date)]: %-${status_length}b[$RED%s$CLR]\n" "$1" "FAIL"
+		result=1
+	elif [ $2 = 0 ]; then
+		printf "[$(format_date)]: %-${status_length}b[$GREEN%s$CLR]\n" "$1" "OK"
+	elif [ $2 -gt 0 ]; then
+		printf "[$(format_date)]: %-${status_length}b[$RED%s$CLR]\n" "$1" "FAIL"
+		result=1
+	fi
+
+	return ${result}
+}
+
+# Function for status on some command in debug mode only
+status_dbg() {
+	[ -z ${DEBUG} ] && return 0
+
+	if [ -z "$1" ] || [ -z "$2" ]; then
+		error "status_dbg(): not found required parameters!"
+		return 1
+	fi
+
+	local length=$(( ${status_length} - 7 ))
+	local result=0
+
+	#debug "status_dbg length: $length"
+
+	if [ $2 = 'OK' ]; then
+		printf "[$(format_date)]: ${GREEN}DEBUG:$CLR %-${length}b[$GREEN%s$CLR]\n" "$1" "OK"
+	elif [ $2 = 'FAIL' ]; then
+		printf "[$(format_date)]: ${GREEN}DEBUG:$CLR %-${length}b[$RED%s$CLR]\n" "$1" "FAIL"
+	elif [ $2 = 0 ]; then
+		printf "[$(format_date)]: ${GREEN}DEBUG:$CLR %-${length}b[$GREEN%s$CLR]\n" "$1" "OK"
+	elif [ $2 -gt 0 ]; then
+		printf "[$(format_date)]: ${GREEN}DEBUG:$CLR %-${length}b[$RED%s$CLR]\n" "$1" "FAIL"
+		result=1
+	fi
+
+	return ${result}
+}
+
+# Function for update status formatting length
+# Example: update_status_length ${files_array}
+update_status_length() {
+	for i in ${@}; do
+		debug "Element length: ${#i}"
+		debug "STATUS_LENGTH before check: ${status_length}"
+		if [ ${#i} -gt $(( ${status_length} - 11 )) ]; then
+			status_length=$(( ${status_length} + $(( ${#i} - ${status_length} + 12 ))  ))
+			debug "NEW STATUS_LENGTH: $status_length"
+		fi
+	done
+}
+
+# Function for checking script dependencies
+check_dependencies() {
+	local result=0
+	local cmd_status
+
+	for i in ${@}; do
 		command -v ${i} >/dev/null 2>&1
-		if [ $? -eq 0 ]; then
-			debug "Check $i ... OK"
-		else
+		cmd_status=$?
+
+		status_dbg "DEPENDENCY: $i" ${cmd_status}
+
+		if [ ${cmd_status} -ne 0 ]; then
 			warning "$i command not available"
 			result=1
 		fi
 	done
+
+	debug "check_dependencies() result: $result"
 
 	return ${result}
 }
@@ -96,164 +173,195 @@ get_group() {
 	echo ${group} | tr '[:upper:]' '[:lower:]'
 }
 
-debug "Parse options"
-while true; do
-	case $1 in
-		-h | --help) debug "Found '${1}' option"
-			usage_help
-			exit 2
-		;;
-		-d | --debug) readonly DEBUG=1
-			debug "Found '${1}' option"
-			shift
-		;;
-		-n) debug "Found '${1}' option"
-			shift
-			readonly NAMESPACE="$1"
-			debug "NAMESPACE set to '${NAMESPACE}'"
-			shift
-		;;
-		--namespace=?* ) value="${1#*=}"; debug "Found '${1}' option"
-			readonly NAMESPACE="$value"
-			debug "NAMESPACE set to '${NAMESPACE}'"
-			shift
-		;;
-		-v | --version) debug "Found '${1}' option"
-			print_version
-			exit 0
-		;;
-		-* | --*) error "Unrecognized option '$1'"
-			exit 2
-		;;
-		*) readonly CLASS_NAME="$1";
-			debug "CLASS_NAME set to '${1}'"
-			readonly FILE_NAME="$CLASS_NAME$CLASS_EXTENSION"
-			debug "FILE_NAME set to '${FILE_NAME}'"
-			break
-		;;
-	esac
-done
+parse_options() {
+	local result=0
+	local long_optarg=''
 
-check_dependencies
+	while getopts :vhdn:-: param; do
+		[ ${param} = '?' ] && found=${OPTARG} || found=${param}
 
-debug "Check that CLASS_NAME not empty"
-if [ -z "${CLASS_NAME}" ]; then
-	error 'CLASS_NAME parameter required'
-	exit 1
-fi
+		debug "Found option '$found'"
 
-debug "Check unit-tests dir: $UNIT_TESTS_DIR"
-if [ ! -x "${UNIT_TESTS_DIR}" ]; then
-	error 'Unit-tests dir not found'
-	exit 1
-fi
+		case ${param} in
+			v ) print_version; exit 0;;
+			h ) usage_help; exit 0;;
+			d ) DEBUG=1;;
+			n ) NAMESPACE=$OPTARG;;
+			- ) case $OPTARG in
+					version      ) print_version; exit 0;;
+					help         ) usage_help; exit 0;;
+					debug        ) DEBUG=1;;
+					namespace=?* ) NAMESPACE="${OPTARG#*=}";;
+					*            ) warning "Illegal option --$OPTARG"; result=2;;
+				esac;;
+			* ) warning "Illegal option -$param"; result=2;;
+		esac
+	done
+	shift $((OPTIND-1))
 
-debug "Check classes dir: ${CLASSES_DIR}"
-if [ ! -x "${CLASSES_DIR}" ]; then
-	error 'Classes dir not found'
-	exit 1
-fi
+	debug "Variable DEBUG: '$DEBUG'"
+	debug "Variable NAMESPACE: '$NAMESPACE'"
+	debug "parse_options() result: $result"
 
-debug "Open dir ${CLASSES_DIR}"
-cd ${CLASSES_DIR}
+	return ${result}
+}
 
-debug "Find relative path to class"
-if [ -z "${NAMESPACE}" ]; then
-	readonly RELATIVE_CLASS_PATH="$(find . -name "${FILE_NAME}" | sed 's/\.\///')"
-else
-	readonly RELATIVE_CLASS_PATH="$(echo "${NAMESPACE}/${FILE_NAME}" | sed 's/Veles\\//' | sed 's/\\/\//g')"
-fi
+function main() {
+	readonly local RED="\e[31m"
+	readonly local GREEN="\e[32m"
+	readonly local YELLOW="\e[33m"
+	readonly local GRAY="\e[38;5;242m"
+	readonly local BOLD="\e[1m"
+	readonly local CLR="\e[0m"
 
-debug "Relative path: '${RELATIVE_CLASS_PATH}'"
+	readonly local CLASSES_DIR='/home/alex/veles'
+	readonly local UNIT_TESTS_DIR='/home/alex/veles/Tests'
+	readonly local CLASS_EXTENSION='.php'
+	readonly local VERSION='1.0.0'
+	readonly local CURRENT_DIR=$(pwd)
+	readonly local TEST_CASE_NAMESPACE='PHPUnit\Framework\TestCase'
 
-debug "Check that RELATIVE_CLASS_PATH is not empty"
-if [ -z "${RELATIVE_CLASS_PATH}" ]; then
-	error "Class '${CLASS_NAME}' not found"
-	cd ${CURRENT_DIR} >/dev/null 2>&1
-	exit 1
-fi
+	readonly local CLASS_NAME=${@:$#}
+	readonly local FILE_NAME="$CLASS_NAME$CLASS_EXTENSION"
 
-debug "Build full class name"
-if [ -z "${NAMESPACE}" ]; then
-	readonly FULL_CLASS_NAME="$(echo "Veles\\${RELATIVE_CLASS_PATH}" | sed s/${CLASS_EXTENSION}// | sed 's/\//\\/g')"
-else
-	readonly FULL_CLASS_NAME="$(echo "${NAMESPACE}\\${CLASS_NAME}")"
-fi
+	local DEBUG=
+	local status_length=60
 
-debug "Full class name: '${FULL_CLASS_NAME}'"
+	parse_options ${@}
 
-debug "Check that FULL_CLASS_NAME is not empty"
-if [ -z "${FULL_CLASS_NAME}" ]; then
-	error "Unable to build full class name"
-	cd ${CURRENT_DIR} >/dev/null 2>&1
-	exit 1
-fi
+	readonly local parse_result=$?
 
-debug "Build relative test path"
-readonly RELATIVE_TEST_PATH="$(echo "Tests/${FULL_CLASS_NAME}" | sed 's/Veles\\//' | sed 's/\\/\//g')Test$CLASS_EXTENSION"
+	[ ${parse_result} = 1 ] && exit 1;
+	[ ${parse_result} = 2 ] && usage_help && exit 2;
 
-debug "Relative test path: '${RELATIVE_TEST_PATH}'"
+	debug "Variable CLASS_NAME: '$CLASS_NAME'"
+	debug "Variable FILE_NAME: '$FILE_NAME'"
 
-debug "Check that RELATIVE_TEST_PATH is not empty"
-if [ -z "${RELATIVE_TEST_PATH}" ]; then
-	error "Unable to build relative test path"
-	cd ${CURRENT_DIR} >/dev/null 2>&1
-	exit 1
-fi
+	check_dependencies phpunit-skelgen printf sed find tr perl || exit 1
 
-debug "Build full test name"
-readonly FULL_TEST_NAME="$(echo "${RELATIVE_TEST_PATH}" | sed s/${CLASS_EXTENSION}// | sed 's/\//\\/g')"
+	debug "Check that CLASS_NAME not empty"
+	if [ -z "${CLASS_NAME}" ]; then
+		error 'CLASS_NAME parameter required'
+		exit 1
+	fi
 
-debug "Full test name: '${FULL_TEST_NAME}'"
+	debug "Check unit-tests dir: $UNIT_TESTS_DIR"
+	if [ ! -x "${UNIT_TESTS_DIR}" ]; then
+		error 'Unit-tests dir not found'
+		exit 1
+	fi
 
-debug "Check that FULL_TEST_NAME is not empty"
-if [ -z "${FULL_TEST_NAME}" ]; then
-	error "Unable to build full test name"
-	cd ${CURRENT_DIR} >/dev/null 2>&1
-	exit 1
-fi
+	debug "Check classes dir: ${CLASSES_DIR}"
+	if [ ! -x "${CLASSES_DIR}" ]; then
+		error 'Classes dir not found'
+		exit 1
+	fi
 
-debug "Build unit test dir"
-readonly UNIT_TEST_DIR="$(echo "${RELATIVE_TEST_PATH}" | sed s/${CLASS_NAME}Test${CLASS_EXTENSION}//)"
+	debug "Open dir ${CLASSES_DIR}"
+	cd ${CLASSES_DIR}
 
-debug "Check that UNIT_TEST_DIR is not empty"
-if [ -z "${UNIT_TEST_DIR}" ]; then
-	error "Unable to build unit-test dir"
-	cd ${CURRENT_DIR} >/dev/null 2>&1
-	exit 1
-fi
+	debug "Find relative path to class"
+	if [ -z "${NAMESPACE}" ]; then
+		readonly RELATIVE_CLASS_PATH="$(find . -name "${FILE_NAME}" | sed 's/\.\///')"
+	else
+		readonly RELATIVE_CLASS_PATH="$(echo "${NAMESPACE}/${FILE_NAME}" | sed 's/Veles\\//' | sed 's/\\/\//g')"
+	fi
 
-debug "Check unit-test dir: ${UNIT_TEST_DIR}"
-if [ ! -x "${UNIT_TEST_DIR}" ]; then
-	create_test_dir
-fi
+	debug "Relative path: '${RELATIVE_CLASS_PATH}'"
 
-debug "Open dir ${CLASSES_DIR}"
-cd "${CLASSES_DIR}" >/dev/null 2>&1
+	debug "Check that RELATIVE_CLASS_PATH is not empty"
+	if [ -z "${RELATIVE_CLASS_PATH}" ]; then
+		error "Class '${CLASS_NAME}' not found"
+		cd ${CURRENT_DIR} >/dev/null 2>&1
+		exit 1
+	fi
 
-phpunit-skelgen --bootstrap='Tests/bootstrap.php' generate-test "${FULL_CLASS_NAME}" "${RELATIVE_CLASS_PATH}" "${FULL_TEST_NAME}" "${RELATIVE_TEST_PATH}"
+	debug "Build full class name"
+	if [ -z "${NAMESPACE}" ]; then
+		readonly FULL_CLASS_NAME="$(echo "Veles\\${RELATIVE_CLASS_PATH}" | sed s/${CLASS_EXTENSION}// | sed 's/\//\\/g')"
+	else
+		readonly FULL_CLASS_NAME="$(echo "${NAMESPACE}\\${CLASS_NAME}")"
+	fi
 
-debug "Create group comment tag"
-readonly group=$(get_group ${RELATIVE_CLASS_PATH})
-readonly group_regex=$(printf "%s %q%s" 's=(Generated by PHPUnit_SkeletonGenerator.*)\s=\1\n \* \@group' "${group}" '\n=')
-perl -pi -e "${group_regex}" "${RELATIVE_TEST_PATH}"
+	debug "Full class name: '${FULL_CLASS_NAME}'"
 
-debug "Create class alias"
-readonly alias_regex=$(printf "%s %q%s" 's=(namespace .*;)\s=\1\n\nuse' "${FULL_CLASS_NAME}" ';\n=')
-perl -pi -e "${alias_regex}" "${RELATIVE_TEST_PATH}"
+	debug "Check that FULL_CLASS_NAME is not empty"
+	if [ -z "${FULL_CLASS_NAME}" ]; then
+		error "Unable to build full class name"
+		cd ${CURRENT_DIR} >/dev/null 2>&1
+		exit 1
+	fi
 
-debug "Change base class"
-perl -pi -e 's/\\PHPUnit_Framework_TestCase/TestCase/' "${RELATIVE_TEST_PATH}"
+	debug "Build relative test path"
+	readonly RELATIVE_TEST_PATH="$(echo "Tests/${FULL_CLASS_NAME}" | sed 's/Veles\\//' | sed 's/\\/\//g')Test$CLASS_EXTENSION"
 
-debug "Add phpunit TestCase alias"
-readonly test_case_regex=$(printf "%s %q%s" 's=(namespace .*;)\s=\1\n\nuse' "${TEST_CASE_NAMESPACE}" ';=')
-perl -pi -e "${test_case_regex}" "${RELATIVE_TEST_PATH}"
+	debug "Relative test path: '${RELATIVE_TEST_PATH}'"
 
-debug "Open tests dir ${UNIT_TESTS_DIR}"
-cd ${UNIT_TESTS_DIR} >/dev/null 2>&1
+	debug "Check that RELATIVE_TEST_PATH is not empty"
+	if [ -z "${RELATIVE_TEST_PATH}" ]; then
+		error "Unable to build relative test path"
+		cd ${CURRENT_DIR} >/dev/null 2>&1
+		exit 1
+	fi
 
-debug "Git checkout to master branch"
-git checkout master
+	debug "Build full test name"
+	readonly FULL_TEST_NAME="$(echo "${RELATIVE_TEST_PATH}" | sed s/${CLASS_EXTENSION}// | sed 's/\//\\/g')"
 
-debug "Open dir ${CURRENT_DIR}"
-cd "${CURRENT_DIR}" >/dev/null 2>&1
+	debug "Full test name: '${FULL_TEST_NAME}'"
+
+	debug "Check that FULL_TEST_NAME is not empty"
+	if [ -z "${FULL_TEST_NAME}" ]; then
+		error "Unable to build full test name"
+		cd ${CURRENT_DIR} >/dev/null 2>&1
+		exit 1
+	fi
+
+	debug "Build unit test dir"
+	readonly UNIT_TEST_DIR="$(echo "${RELATIVE_TEST_PATH}" | sed s/${CLASS_NAME}Test${CLASS_EXTENSION}//)"
+
+	debug "Check that UNIT_TEST_DIR is not empty"
+	if [ -z "${UNIT_TEST_DIR}" ]; then
+		error "Unable to build unit-test dir"
+		cd ${CURRENT_DIR} >/dev/null 2>&1
+		exit 1
+	fi
+
+	debug "Check unit-test dir: ${UNIT_TEST_DIR}"
+	if [ ! -x "${UNIT_TEST_DIR}" ]; then
+		create_test_dir
+	fi
+
+	debug "Open dir ${CLASSES_DIR}"
+	cd "${CLASSES_DIR}" >/dev/null 2>&1
+
+	phpunit-skelgen --bootstrap='Tests/bootstrap.php' generate-test "${FULL_CLASS_NAME}" "${RELATIVE_CLASS_PATH}" "${FULL_TEST_NAME}" "${RELATIVE_TEST_PATH}"
+
+	debug "Create group comment tag"
+	readonly group=$(get_group ${RELATIVE_CLASS_PATH})
+	readonly group_regex=$(printf "%s %q%s" 's=(Generated by PHPUnit_SkeletonGenerator.*)\s=\1\n \* \@group' "${group}" '\n=')
+	perl -pi -e "${group_regex}" "${RELATIVE_TEST_PATH}"
+
+	debug "Create class alias"
+	readonly alias_regex=$(printf "%s %q%s" 's=(namespace .*;)\s=\1\n\nuse' "${FULL_CLASS_NAME}" ';\n=')
+	perl -pi -e "${alias_regex}" "${RELATIVE_TEST_PATH}"
+
+	debug "Change base class"
+	perl -pi -e 's/\\PHPUnit_Framework_TestCase/TestCase/' "${RELATIVE_TEST_PATH}"
+
+	debug "Add phpunit TestCase alias"
+	readonly test_case_regex=$(printf "%s %q%s" 's=(namespace .*;)\s=\1\n\nuse' "${TEST_CASE_NAMESPACE}" ';=')
+	perl -pi -e "${test_case_regex}" "${RELATIVE_TEST_PATH}"
+
+	debug "Open tests dir ${UNIT_TESTS_DIR}"
+	cd ${UNIT_TESTS_DIR} >/dev/null 2>&1
+
+	debug "Git checkout to master branch"
+	git checkout master
+
+	debug "Open dir ${CURRENT_DIR}"
+	cd "${CURRENT_DIR}" >/dev/null 2>&1
+}
+
+main ${@}
+
+exit 0
